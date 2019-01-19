e2c9c83bf30b78a9bc6bc5fb44f347d08405ff1d
mkcacchione <45609817+mkcacchione@users.noreply.github.com>
2019-01-19 14:39:24 +0100

    Add files via upload

---
+++ first_function.py
@@ -0,0 +1,28 @@
+Python 3.7.2 (v3.7.2:9a3ffc0492, Dec 24 2018, 02:44:43) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def f(x):
+	return x*3+(2*7)
+
+>>> x=f(3)
+>>> print (x)
+23
+>>> if x==15
+SyntaxError: invalid syntax
+>>> if x == 15:
+	print("x is a small numner")
+elif x%2==0:
+	print("x is even")
+else x>15:
+	
+SyntaxError: invalid syntax
+>>> if x == 15:
+	print ("x is a small numner")
+elif x % 2 == 0:
+	print ("x is even")
+else:
+	print ("x is not small or even")
+
+	
+x is not small or even
+>>> 
