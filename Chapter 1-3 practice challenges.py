c71c95cd4de2ab322bc4197daf52b493218a2869
mkcacchione <45609817+mkcacchione@users.noreply.github.com>
2019-02-12 13:38:55 +0000

    Add files via upload

---
+++ Chapter 1-3 practice challenges.py
@@ -0,0 +1,41 @@
+#question 1 page 6
+checklist = ("shower", "get ready", "school", "home", "coder class", "sleep")
+print(checklist)
+
+#question 1 page 11
+print ("my name is MK.  I am excited to learn python")
+
+#question 1 page 44
+print("three", "different", "strings")
+
+#question 2 page 44
+x=20
+if x<10:
+    print("x is less than 10")
+else:
+    print("x is not less than 10")
+
+#question 3 page 44
+if x<=10:
+    print("x is not greater than 10")
+elif x>10 and x<25:
+    print ("x is more than 10 but less than 25")
+else:
+    print("x is at least 25")
+
+#question 4 page 44
+a=10
+b=5
+print(a%b)
+
+#question 5 page 44
+print(a//b)
+
+#question 6 page 44
+age= input("enter your age:")
+age = int(age)
+retirement = 65-age
+if retirement<=10:
+    print("you can retire soon")
+else:
+    print("you have to keep working")
